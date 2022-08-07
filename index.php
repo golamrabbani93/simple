d4c9d83bdd347f122cdbe832ed594f13fb6161da
golamrabbani93 <golamrabbani9373@gmail.com>
2022-08-07 22:55:02 +0600

    create and call blogs part

--- index.php
+++ index.php
@@ -8,52 +8,9 @@
         </div>
         <!-- Banner End Here -->
 
-        //*Call Services-part
+        <!-- //*Call Services-part -->
         <?php get_template_part( 'Template-parts/content', 'services' );?>
-        <!-- Blog Start Here -->
-        <div class="blogs">
-            <div class="blog-left">
-                <h4>latest blog</h4>
-                <div class="blogs">
-                    <div class="single-blog">
-                        <h4><a href="">Blog Title</a></h4>
-                        <img src="<?php echo get_template_directory_uri();?>/assets/img/blog2.jpg" alt="">
-                        <div class="blog-meta">
-                            <a href="">Admin</a><a href="">3 July</a><a href="">Food</a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsa et vitae necessitatibus delectus nostrum? Sapiente quasi iste quibusdam facilis?</p>
-                        <a href="">read more</a>
-                    </div>    
-                    <div class="single-blog">
-                        <h4><a href="">Blog Title</a></h4>
-                        <img src="<?php echo get_template_directory_uri();?>/assets/img/blog3.jpg" alt="">
-                        <div class="blog-meta">
-                            <a href="">Admin</a><a href="">3 July</a><a href="">Food</a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsa et vitae necessitatibus delectus nostrum? Sapiente quasi iste quibusdam facilis?</p>
-                        <a href="">read more</a>
-                    </div> 
-                    <div class="single-blog">
-                        <h4><a href="">Blog Title</a></h4>
-                        <img src="<?php echo get_template_directory_uri();?>/assets/img/blog3.jpg" alt="">
-                        <div class="blog-meta">
-                            <a href="">Admin</a><a href="">3 July</a><a href="">Food</a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsa et vitae necessitatibus delectus nostrum? Sapiente quasi iste quibusdam facilis?</p>
-                        <a href="">read more</a>
-                    </div> 
-                    <div class="single-blog">
-                        <h4><a href="">Blog Title</a></h4>
-                        <img src="<?php echo get_template_directory_uri();?>/assets/img/blog4.jpg" alt="">
-                        <div class="blog-meta">
-                            <a href="">Admin</a><a href="">3 July</a><a href="">Food</a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ipsa et vitae necessitatibus delectus nostrum? Sapiente quasi iste quibusdam facilis?</p>
-                        <a href="">read more</a>
-                    </div>                 
-                </div>
-            </div>
-             <?php get_sidebar(); ?>
-        </div>
-        <!-- Blog End Here -->
+        <!--//*Call Blogs-part -->
+        <?php get_template_part( 'Template-parts/content', 'blogs' );?>
+        
        <?php get_footer(); ?>
