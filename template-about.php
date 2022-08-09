332efb6cff3ff783c49989089d68b01e0e020d3a
golamrabbani93 <golamrabbani9373@gmail.com>
2022-08-09 20:37:42 +0600

    About page sidebar dynamic

--- template-about.php
+++ template-about.php
@@ -22,16 +22,7 @@
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
             </div>
             <div class="about-menu">
-                <div class="single-sidebar">
-                    <h4>Latest Posts</h4>
-                    <ul>
-                        <li><a href="">Post Title</a></li>
-                        <li><a href="">Post Title</a></li>
-                        <li><a href="">Post Title</a></li>
-                        <li><a href="">Post Title</a></li>
-                        <li><a href="">Post Title</a></li>
-                    </ul>
-                </div>
+            <?php get_sidebar(); ?>
             </div>
         </div>
   <?php 
