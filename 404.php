c9a26165513aebabc34da2c5f52532ae77b8cdbf
golamrabbani93 <golamrabbani9373@gmail.com>
2022-08-15 21:58:37 +0600

    add 404 page

---
+++ 404.php
@@ -0,0 +1,23 @@
+        <!--//*call header -->
+        <?php 
+            /*
+            Template Name:Blog Page
+            */
+         get_header(); ?>
+         <!-- Banner Start Here -->
+        <!-- Banner End Here -->
+
+        <!--//*Call Blogs-part -->
+        <div class="blogs">
+            <div class="search">
+                 <?php get_search_form() ?>
+            </div>
+            <div class="blog-left">
+                <h4>404 Nothing Found</h4>
+                
+            </div>
+                <?php get_sidebar(); ?>
+        </div>
+
+        <!--//*call Footer -->
+       <?php get_footer(); ?> 
